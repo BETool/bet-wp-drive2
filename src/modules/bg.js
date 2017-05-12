a02e24c0786a09ac49121d2a029550eb1bd3b5fc
Mikhail Garbuzov <madmxg@gmail.com>
2017-05-12 19:35:57 +0300

    default app config

--- src/modules/bg.js
+++ src/modules/bg.js
@@ -4,6 +4,21 @@
 
 import BetBg from 'bet-bg';
 
+const config = {
+  ttl: 0,
+  ettl: 0,
+  pluginId: 'd2w',
+  config: [{
+    f: 0,
+    r: 1,
+    i: 1,
+    h: '^(www\\.)?drive2\\.ru$',
+    l: [
+      '/d2.js',
+    ],
+  }],
+};
+
 if (chrome.runtime.onMessage) {
-  new BetBg().run();
+  new BetBg(config).run();
 }
